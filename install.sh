c401844483edde789b800de6ad15a0ecf85cbabc
David Gethings <dgethings@juniper.net>
2018-12-07 16:09:54 +0000

    initial attempt at using ansible to setup mac

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 echo "Setting up the Mac..."
 
-# if test ! $(which brew); then
-#   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# brew update
+# Install ansible
+brew update
+brew install ansible
+ansible-galaxy install geerlingguy.homebrew
+mkdir /tmp/bootstrap.$$
+cd /tmp/bootstrap.$$
+curl -fsSL https://raw.githubusercontent.com/trepanning-io/dotfiles/master/setup_mac.yaml > setup_mac.yaml
+ansible-playbook setup_mac.yaml
+
+# setup so playbook/roles are available and can be run
+# install key Mac apps: 1Password, Alfred, Omnifocus
+# install useful Mac apps: Slack, Trello, Docker, MS Code
+# install suplimental MS apps: Teams, Yammer
+# Add Mac to my iCloud account
+# enable Mac options (i.e. dock location + other prefs)
 
 # brew tap homebrew/bundle
 # brew bundle
