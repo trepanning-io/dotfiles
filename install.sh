d57f620ad2ecd3ef738f82078d1a89fcdff6777e
David Gethings <dgethings@juniper.net>
2018-12-07 18:08:19 +0000

    mrew brewfile exec tweaking

--- install.sh
+++ install.sh
@@ -4,14 +4,10 @@ if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-mkdir /tmp/bootstrap.$$
-cd /tmp/bootstrap.$$
+mkdir -p /tmp/bootstrap.$$
 curl -fsSL https://raw.githubusercontent.com/trepanning-io/dotfiles/master/Brewfile > Brewfile
-
-brew bundle
-cd ~
+brew bundle /tmp/bootstrap.$$/Brewfile
 rm -rf /tmp/bootstrap.$$
-# setup so playbook/roles are available and can be run
 # install key Mac apps: 1Password, Alfred, Omnifocus
 # install useful Mac apps: Slack, Trello, Docker, MS Code
 # install suplimental MS apps: Teams, Yammer
