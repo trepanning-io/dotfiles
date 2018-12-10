ee03fdd75afc442ce3ad907192586485103edc8b
David <dgethings@juniper.net>
2018-12-10 16:03:36 +0000

    run osx_prefs.sh as part of install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,23 @@ git config --global user.name $(id -F)
 git config --global user.email "dgethings@juniper.net"
 git config --global credential.helper osxkeychain
 
+if [ ! -d "${HOME}/git" ]; then
+  mkdir ~/git
+fi
+
+if [ ! -d "${HOME}/git/dotfiles" ]; then
+  (
+    cd ~/git
+    git clone https://github.com/trepanning-io/dotfiles.git
+  )
+fi
+
+echo "Configuring Mac prefs..."
+(
+  cd ~/git/dotfiles
+  ./osx_prefs.sh
+)
+
 # mkdir ~/git || false
 # cd ~/git
 # if ! -d dotfiles; then
