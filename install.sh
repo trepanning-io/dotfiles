a7f37219f7d8546a9b22b60cf66d3b0bf542f219
David Gethings <trepanning.io@gmail.com>
2020-12-31 09:21:55 +0000

    improve install robustness

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ function install_brew() {
 
 function setup_repo() {
     brew install git
-    mkdir ${HOME}/git
+    [ -d ${HOME}/git ] || mkdir ${HOME}/git
     cd ${HOME}/git
-    git clone --depth 1 https://github.com/trepanning-io/dotfiles.git
+    if [ -d dotfiles ]; then
+        cd dotfiles
+        git pull
+    else
+        git clone --depth 1 https://github.com/trepanning-io/dotfiles.git
+    fi
 }
 
 function install_apps() {
@@ -16,11 +21,16 @@ function install_apps() {
     brew bundle
 }
 
+function install_omz() {
+    cd ${HOME}
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+
 function apply_dotfiles() {
     cd ${HOME}/git/dotfiles
     files="$@"
     for file in "${files[@]}"; do
-        echo $file
+        ln -s $PWD/$file ${HOME}/.$file
     done
 }
 
@@ -29,7 +39,8 @@ darwin*)
     install_brew
     setup_repo
     install_apps
-    files=(agignore alacritty.yml)
+    install_omz
+    files=(agignore alacritty.yml aliases gitconfig gitignore hyper.js p10k.zsh zshenv zshrc)
     apply_dotfiles "${files[@]}"
     # install other things (i.e. helmenv)
     # install versions of langs/apps using *env commands
